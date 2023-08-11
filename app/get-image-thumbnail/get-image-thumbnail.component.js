76de2ff158b8f604b95a9ac801dfa812f6be4ec4
pavlo.batsemakin <pavlo.batsemakin@unity-bars.com>
2023-08-11 10:21:39 +0300

    The obtain thumbnail logic was completed

--- app/get-image-thumbnail/get-image-thumbnail.component.js
+++ app/get-image-thumbnail/get-image-thumbnail.component.js
@@ -1,8 +1,38 @@
 angular.
   module('getImageThumbnail').
   component('getImageThumbnail', {
+    
     templateUrl: 'get-image-thumbnail/get-image-thumbnail.template.html',
-    controller: function GetImageThumbnailController() {
+    
+    controller: function GetImageThumbnailController($http, $scope) {
         
+      $scope.id = null;
+      $scope.imageUrl = null;
+
+      $scope.get100 = function () {
+        if ($scope.id) {
+          $http.get(`https://localhost:7050/api/images/${$scope.id}/size/100`,)
+            .then(function success(response) {
+              $scope.imageUrl = `https://localhost:7050/api/images/${$scope.id}/size/100`;
+            }, function (error) {
+              console.error(error);
+              $scope.imageUrl = null;
+            });
+        }
+      }
+
+      $scope.get300 = function () {
+        if ($scope.id) {
+          $http.get(`https://localhost:7050/api/images/${$scope.id}/size/300`,)
+          .then(function success(response) {
+            $scope.imageUrl = `https://localhost:7050/api/images/${$scope.id}/size/300`;
+          }, function (error) {
+            console.error(error);
+            $scope.imageUrl = null;
+          });
+        }
+      }
+
     }
+  
   });
