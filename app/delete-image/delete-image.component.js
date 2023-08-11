bc52d729ac1f270a673c9f4dc83b65ab732cc5c0
pavlo.batsemakin <pavlo.batsemakin@unity-bars.com>
2023-08-11 10:56:47 +0300

    The deletion logic was completed

--- app/delete-image/delete-image.component.js
+++ app/delete-image/delete-image.component.js
@@ -1,8 +1,23 @@
 angular.
   module('deleteImage').
   component('deleteImage', {
+
     templateUrl: 'delete-image/delete-image.template.html',
-    controller: function DeleteImageController() {
+    
+    controller: function DeleteImageController($http, $scope) {
+
+      $scope.id = null;
+
+      $scope.click = function () {
+        if ($scope.id) {
+          $http.delete(`https://localhost:7050/api/images/${$scope.id}`,)
+            .then(function success(response) {
+             alert(`The image with id ${$scope.id} was succesfully removed`);
+            }, function (error) {
+              console.error(error);
+            });
+        }
+      }
         
     }
   });
