3eeed76d7eb32ff805f5c0a325de9b3d16882f0f
Rhoxolan <pba17mail@gmail.com>
2024-02-15 17:04:49 +0200

    The auth component was supplemented

--- app/auth/auth.component.js
+++ app/auth/auth.component.js
@@ -6,15 +6,67 @@ angular.
 
     controller: function AuthController($http, $scope) {
 
+      var tokenKey = "image_wizard_access_token";
+      var userNameKey = "image_director_username";
       $scope.userName = null;
       $scope.isAuthorized = false;
+      $scope.hasError = false;
+      $scope.errorMessage = null;
+
+      $scope.init = function () {
+        let token = sessionStorage.getItem(tokenKey);
+        let userName = sessionStorage.getItem(userNameKey);
+        if(token && userName){
+          $scope.isAuthorized = true;
+          $scope.userName = userName;
+        }
+      };
+
+      $scope.register = function () {
+        if ($scope.userName && document.getElementById("Password").value > 0) {
+          $http.post("https://localhost:7050/api/register", {
+            Login: $scope.userName,
+            Password: document.getElementById("Password").value
+          })
+            .then(function success(response) {
+              document.getElementById("Password").value = '';
+              sessionStorage.setItem(userNameKey, $scope.userName);
+              sessionStorage.setItem(tokenKey, response.data.token);
+              $scope.hasError = false;
+              $scope.errorMessage = null;
+            }, function (error) {
+              console.error(error);
+              $scope.hasError = true;
+              $scope.errorMessage = error.data;
+            });
+        }
+      }
 
       $scope.login = function () {
-        
+        if ($scope.userName && document.getElementById("Password").value > 0) {
+          $http.post("https://localhost:7050/api/login", {
+            Login: $scope.userName,
+            Password: document.getElementById("Password").value
+          })
+            .then(function success(response) {
+              document.getElementById("Password").value = '';
+              sessionStorage.setItem(userNameKey, $scope.userName);
+              sessionStorage.setItem(tokenKey, response.data.token);
+              $scope.hasError = false;
+              $scope.errorMessage = null;
+            }, function (error) {
+              console.error(error);
+              $scope.hasError = true;
+              $scope.errorMessage = error.data;
+            });
+        }
       }
 
       $scope.logout = function () {
-        
+        sessionStorage.removeItem(tokenKey);
+        sessionStorage.removeItem(userNameKey);
+        $scope.userName = null;
+        $scope.isAuthorized = false;
       }
 
     }
