7ad6e5e50214c032a1c589db2d2209a8d80ca2ec
Rhoxolan <pba17mail@gmail.com>
2024-03-03 23:34:39 +0200

    The get image thumbnail component was modsfied

--- app/get-image-thumbnail/get-image-thumbnail.component.js
+++ app/get-image-thumbnail/get-image-thumbnail.component.js
@@ -1,48 +1,54 @@
 angular.
   module('getImageThumbnail').
   component('getImageThumbnail', {
-    
+
     templateUrl: 'get-image-thumbnail/get-image-thumbnail.template.html',
-    
-    controller: function GetImageThumbnailController($http, $scope) {
-        
+
+    controller: function GetImageThumbnailController($http, $scope, authService) {
+
       $scope.id = null;
       $scope.imageUrl = null;
       $scope.hasError = false;
       $scope.errorMessage = null;
 
-      $scope.get100 = function () {
+      var getImage = function (size) {
         if ($scope.id) {
-          $http.get(`https://localhost:7050/api/images/${$scope.id}/size/100`,)
+          let url = `https://localhost:7050/api/images/${$scope.id}/size/${size}`;
+          let config = {};
+          var token = authService.getToken();
+          if (!token) {
+            return;
+          }
+          config.headers = {
+            'Authorization': 'Bearer ' + token
+          };
+
+          $http.get(url, config)
             .then(function success(response) {
-              $scope.imageUrl = `https://localhost:7050/api/images/${$scope.id}/size/100`;
+              $scope.imageUrl = `https://localhost:7050/api/images/${$scope.id}/size/${size}`;
               $scope.hasError = false;
               $scope.errorMessage = null;
             }, function (error) {
               console.error(error);
               $scope.imageUrl = null;
+              if (error.status === 401) {
+                authService.logout();
+                return;
+              }
               $scope.hasError = true;
               $scope.errorMessage = error.data.title;
             });
         }
       }
 
+      $scope.get100 = function () {
+        getImage(100);
+      }
+
       $scope.get300 = function () {
-        if ($scope.id) {
-          $http.get(`https://localhost:7050/api/images/${$scope.id}/size/300`,)
-          .then(function success(response) {
-            $scope.imageUrl = `https://localhost:7050/api/images/${$scope.id}/size/300`;
-            $scope.hasError = false;
-            $scope.errorMessage = null;
-          }, function (error) {
-            console.error(error);
-            $scope.imageUrl = null;
-            $scope.hasError = true;
-            $scope.errorMessage = error.data.title;
-          });
-        }
+        getImage(300);
       }
 
     }
-  
+
   });
