6b8d5b9bbb04a9fb83c071f01d379b7bc1922029
Rhoxolan <pba17mail@gmail.com>
2024-03-04 23:15:53 +0200

    The work on the auth impelemning was complted

--- app/delete-image/delete-image.component.js
+++ app/delete-image/delete-image.component.js
@@ -4,7 +4,7 @@ angular.
 
     templateUrl: 'delete-image/delete-image.template.html',
     
-    controller: function DeleteImageController($http, $scope) {
+    controller: function DeleteImageController($http, $scope, authService) {
 
       $scope.id = null;
       $scope.hasError = false;
@@ -12,13 +12,28 @@ angular.
 
       $scope.click = function () {
         if ($scope.id) {
-          $http.delete(`https://localhost:7050/api/images/${$scope.id}`,)
+          let url = `https://localhost:7050/api/images/${$scope.id}`;
+          let config = {};
+          var token = authService.getToken();
+          if (!token) {
+            return;
+          }
+          config.headers = {
+            'Authorization': 'Bearer ' + token
+          };
+
+
+          $http.delete(url, config)
             .then(function success(response) {
              alert(`The image with id ${$scope.id} was succesfully removed`);
              $scope.hasError = false;
              $scope.errorMessage = null;
             }, function (error) {
               console.error(error);
+              if (error.status === 401) {
+                authService.logout();
+                return;
+              }
               $scope.hasError = true;
               $scope.errorMessage = error.data.title;
             });
